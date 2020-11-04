ca647dfa6a2e9d53d763beab48c8d174b4cde61d
Muhammad Rende Ramadhani <renderamadhani1@gmail.com>
2020-11-04 18:13:30 +0700

    Revisi LAB1&2

---
+++ tugas python/pt5.py
@@ -0,0 +1,18 @@
+print("===BIODATAKU===")
+print()
+namalengkap=input("masukkan nama lengkap anda : ")
+namapanggilan=input("masukkan nama panggilan anda : ")
+npm=int(input("masukkan npm anda : "))
+tempatlahir=input("tempat lahir anda : ")
+tahun=int(input("masukkan tahun lahir anda : "))
+telepon=int(input("masukkan no.telepon anda : "))
+alamatlengkap=input("masukkan alamat lengkap anda : ")
+
+dob = 2020-tahun
+
+print("\n\n===BIODATA SAYA===")
+print("Assalamualaikum")
+print()
+print(f"let me introduce my self. My name is" ,namalengkap, " but you can call me" ,namapanggilan, ". My npm is" ,npm, ". I was born in" ,tempatlahir, "and i am" ,dob, "years old. i am very glad if you want to invit my house in" ,alamatlengkap, " . so, don't forget to call me before with the number" ,telepon, ".")
+print()
+print("Thank you")
